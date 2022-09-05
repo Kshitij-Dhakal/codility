951465ddf8ffcc77567c0bc3ca62e607f3b7b65e
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-09-05 13:46:28 +0545

    Passing cars solution

---
+++ src/main/java/codility/prefixsum/PassingCars.java
@@ -0,0 +1,39 @@
+package codility.prefixsum;
+
+/**
+ * <a href="https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/">Problem Definition</a>
+ */
+public class PassingCars {
+    public int solution(int[] A) {
+        int[] p = new int[A.length + 1];
+        for (int i = 0; i < A.length; i++) {
+            p[i + 1] = p[i] + A[i];
+        }
+        int total = 0;
+        if (p[A.length] == 0 || p[A.length] == A.length) {
+            return 0;
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == 0) {
+                total += p[A.length] - p[i];
+            }
+            if (total > 1_000_000_000)
+                return -1;
+        }
+        return total;
+    }
+
+
+    public static void main(String[] args) {
+        PassingCars p = new PassingCars();
+        System.out.println(p.solution(new int[]{0, 0, 1, 1}));
+        System.out.println(p.solution(new int[]{0, 1, 0, 1, 1}));
+        System.out.println(p.solution(new int[]{1, 0, 0, 1, 1, 1}));
+        System.out.println(p.solution(new int[]{0, 0, 0, 0}));
+        System.out.println(p.solution(new int[]{1, 1, 1, 1}));
+        System.out.println(p.solution(new int[]{1, 1}));
+        System.out.println(p.solution(new int[]{0, 0}));
+        System.out.println(p.solution(new int[]{0}));
+        System.out.println(p.solution(new int[]{1}));
+    }
+}
