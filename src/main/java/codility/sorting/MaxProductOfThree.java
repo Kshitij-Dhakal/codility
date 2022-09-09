466be42a2b9989d18f3e02aac95b2cb7e5aaa9ee
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-09-09 20:11:49 +0545

    max product of three impl

---
+++ src/main/java/codility/sorting/MaxProductOfThree.java
@@ -0,0 +1,26 @@
+package codility.sorting;
+
+import java.util.Arrays;
+
+public class MaxProductOfThree {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        Arrays.sort(A);
+        int length = A.length;
+        if (length == 0) {
+            return 1;
+        } else if (length == 1) {
+            return A[0];
+        } else if (length == 2) {
+            return A[0] * A[1];
+        } else {
+            return Math.max(A[length - 3] * A[length - 2] * A[length - 1],
+                    A[0] * A[1] * A[length - 1]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
