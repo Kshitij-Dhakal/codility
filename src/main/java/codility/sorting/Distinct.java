f8b68baa68ac4b3b42f187d87f65e70101d3def0
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-09-09 09:38:00 +0545

    distinct count

---
+++ src/main/java/codility/sorting/Distinct.java
@@ -0,0 +1,24 @@
+package codility.sorting;
+
+import java.util.Arrays;
+
+public class Distinct {
+    public int solution(int[] A) {
+        if (A.length == 0) {
+            return 0;
+        }
+        int count = 1;
+        Arrays.sort(A);
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] != A[i - 1]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Distinct d = new Distinct();
+        System.out.println(d.solution(new int[]{2, 1, 1, 2, 3, 1}));
+    }
+}
