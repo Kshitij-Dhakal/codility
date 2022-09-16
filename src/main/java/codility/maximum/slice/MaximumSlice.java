f9a604d82006eba806369a39646f5c1cec9bc3f7
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-09-16 17:51:33 +0545

    minor bug fix

--- src/main/java/codility/maximum/slice/MaximumSlice.java
+++ src/main/java/codility/maximum/slice/MaximumSlice.java
@@ -8,7 +8,7 @@ public class MaximumSlice {
         //maximum sum of slice ending at i = maxEndSum(i)
         //maxEndSum(i) = max(0, maxEndSum(i-1) + arr[i])
         for (int i : arr) {
-            maxEndSum = Math.max(0, maxSliceSum + i);
+            maxEndSum = Math.max(0, maxEndSum + i);
             maxSliceSum = Math.max(maxEndSum, maxSliceSum);
         }
         return maxSliceSum;
