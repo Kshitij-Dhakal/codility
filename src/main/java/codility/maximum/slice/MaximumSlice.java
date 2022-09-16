153228673b9fb013cfebf26e93a82bd6b5ee56e9
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-09-16 17:48:20 +0545

    max profit solution

--- src/main/java/codility/maximum/slice/MaximumSlice.java
+++ src/main/java/codility/maximum/slice/MaximumSlice.java
@@ -6,10 +6,7 @@ public class MaximumSlice {
         int maxSliceSum = 0;
 
         //maximum sum of slice ending at i = maxEndSum(i)
-        //maxEndSum(i) = max(0, maxEndSum(i-1))
-        //example,
-        //let's say maxEndSum at i-1 is 10 and ith element is 11. Then max end sum at i is 21.
-        //let's say maxEndSum at i-1 is 10 and ith element is -11. Then max end sum at i is obviously 1.
+        //maxEndSum(i) = max(0, maxEndSum(i-1) + arr[i])
         for (int i : arr) {
             maxEndSum = Math.max(0, maxSliceSum + i);
             maxSliceSum = Math.max(maxEndSum, maxSliceSum);
