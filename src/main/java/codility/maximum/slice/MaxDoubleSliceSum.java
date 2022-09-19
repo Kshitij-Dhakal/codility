f7c5f4780e226e921d441c9a7318b6e42e678f58
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-09-19 09:37:53 +0545

    max double slice problem

---
+++ src/main/java/codility/maximum/slice/MaxDoubleSliceSum.java
@@ -0,0 +1,5 @@
+package codility.maximum.slice;
+
+public class MaxDoubleSliceSum {
+    // TODO: 9/19/22  
+}
--- src/main/java/codility/maximum/slice/MaximumSlice.java
+++ src/main/java/codility/maximum/slice/MaximumSlice.java
@@ -3,12 +3,12 @@ package codility.maximum.slice;
 public class MaximumSlice {
     private int maximumSliceSum(int[] arr) {
         int maxEndSum = 0;
-        int maxSliceSum = 0;
+        int maxSliceSum = Integer.MIN_VALUE;
 
         //maximum sum of slice ending at i = maxEndSum(i)
         //maxEndSum(i) = max(0, maxEndSum(i-1) + arr[i])
         for (int i : arr) {
-            maxEndSum = Math.max(0, maxEndSum + i);
+            maxEndSum = Math.max(i, maxEndSum + i);
             maxSliceSum = Math.max(maxEndSum, maxSliceSum);
         }
         return maxSliceSum;
