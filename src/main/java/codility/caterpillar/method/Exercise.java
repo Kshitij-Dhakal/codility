35a58f73a79831c2de762bcea8ea76285a840b84
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-10-07 20:52:25 +0545

    caterpillar method exercise impl

---
+++ src/main/java/codility/caterpillar/method/Exercise.java
@@ -0,0 +1,24 @@
+package codility.caterpillar.method;
+
+public class Exercise {
+    public boolean caterpillarMethod(int[] a, int s) {
+        int sum = 0;
+        int i = 0;
+        int from = 0;
+        while (i < a.length - 1) {
+            sum += a[i++];
+            if (sum == s) {
+                return true;
+            } else if (sum > s) {
+                sum = sum - a[from];
+                from++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Exercise ex = new Exercise();
+        System.out.println(ex.caterpillarMethod(new int[]{6, 2, 7, 4, 1, 3, 6}, 12));
+    }
+}
