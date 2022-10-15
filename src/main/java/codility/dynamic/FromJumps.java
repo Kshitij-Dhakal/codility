47667e84bf6802faffa453816eadd19c889f156e
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-10-15 14:43:55 +0545

    dynamic programming impl

---
+++ src/main/java/codility/dynamic/FromJumps.java
@@ -0,0 +1,20 @@
+package codility.dynamic;
+
+public class FromJumps {
+    public int solution(int[] s, int k, int q) {
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+        for (int i = 1; i < k + 1; i++) {
+            for (int j : s) {
+                if (j <= i) {
+                    dp[i] = (dp[i] + dp[i - j]) % q;
+                }
+            }
+        }
+        return dp[k];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FromJumps().solution(new int[]{2, 3}, 10, 10));
+    }
+}
