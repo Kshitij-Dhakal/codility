94d7f0bff13649cd214d78fe8f499e89db167636
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-10-16 18:40:00 +0545

    coin exchange memory optimization

--- src/main/java/codility/dynamic/CoinExchange.java
+++ src/main/java/codility/dynamic/CoinExchange.java
@@ -13,23 +13,21 @@ public class CoinExchange {
     }
 
     public int getChange(int[] coins, int change) {
-        int[][] d = new int[coins.length + 1][change + 1];
+        int[] d = new int[change + 1];
         for (int i = 0; i < coins.length + 1; i++) {
             for (int j = 0; j < change + 1; j++) {
                 if (j == 0) {
-                    d[i][j] = 0;
+                    d[j] = 0;
                 } else if (i == 0) {
-                    d[i][j] = Integer.MAX_VALUE;
+                    d[j] = Integer.MAX_VALUE;
                 } else {
                     int c = coins[i - 1];
-                    if (j - c < 0) {
-                        d[i][j] = d[i - 1][j];
-                    } else {
-                        d[i][j] = Math.min(d[i - 1][j], d[i][j - c] + 1);
+                    if (j - c >= 0) {
+                        d[j] = Math.min(d[j], d[j - c] + 1);
                     }
                 }
             }
         }
-        return d[coins.length][change];
+        return d[change];
     }
 }
