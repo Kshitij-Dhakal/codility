b6235511bcf1e94633c98fc38cce7cd4c8679ecb
Kshitij Dhakal <dhakalkshitij@gmail.com>
2022-10-16 18:35:27 +0545

    refactoring

--- src/main/java/codility/dynamic/CoinExchangeIterative.java
+++ src/main/java/codility/dynamic/CoinExchangeIterative.java
@@ -5,15 +5,17 @@ public class CoinExchangeIterative {
         int[][] d = new int[coins.length + 1][change + 1];
         for (int i = 0; i < coins.length + 1; i++) {
             for (int j = 0; j < change + 1; j++) {
-                int c = i == 0 ? 0 : coins[i - 1];
                 if (j == 0) {
                     d[i][j] = 0;
                 } else if (i == 0) {
                     d[i][j] = Integer.MAX_VALUE;
-                } else if (c > j) {
-                    d[i][j] = d[i - 1][j];
                 } else {
-                    d[i][j] = Math.min(d[i][j - c] + 1, d[i - 1][j]);
+                    int c = coins[i - 1];
+                    if (j - c < 0) {
+                        d[i][j] = d[i - 1][j];
+                    } else {
+                        d[i][j] = Math.min(d[i - 1][j], d[i][j - c] + 1);
+                    }
                 }
             }
         }
